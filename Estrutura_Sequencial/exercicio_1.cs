67a7b9b4a9323913e795a1a843fdc845c7fb8448
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-09 10:32:20 -0300

    exercicio_1

---
+++ Estrutura_Sequencial/exercicio_1.cs
@@ -0,0 +1,11 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+using System;
+
+class Exercicio_1{
+
+    static void Main(){
+
+        Console.WriteLine("Olá, mundo !!!");
+    }
+}
