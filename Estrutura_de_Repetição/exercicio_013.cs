0efe38a13ee03515903f0b9f42784dda17ba4505
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-09-27 11:23:39 -0300

    Update exercicio_013.cs

--- Estrutura_de_Repetição/exercicio_013.cs
+++ Estrutura_de_Repetição/exercicio_013.cs
@@ -6,23 +6,48 @@ class exercicio_013{
 
     static void Main(){
 
-        int base_num = 0;
+        double base_num = 0;
         int expoente = 0;
-        int resultado = 0;
+        double resultado = 0;
         int i = 0;
 
         Console.WriteLine("Informe a base: ");
-        base_num = int.Parse(Console.ReadLine());
+        base_num = double.Parse(Console.ReadLine());
         Console.WriteLine("Informe o expoente: ");
         expoente = int.Parse(Console.ReadLine());
 
         resultado = base_num;
+        Console.Write(base_num + " elevado " + expoente + " = ");
 
-        for (i = 1; i < expoente; i++){
+        if (expoente > 0){
+
+            i = 1;
+
+            while (i != expoente){
+                
+                i++;
+                resultado *= base_num;
+            }
             
-            resultado *= base_num;
+        }
+        else if (expoente < 0){
+
+            i = 1;
+            expoente *= -1;
+
+            while (i != expoente){
+                
+                i++;
+                resultado *= base_num;
+            }
+            
+            resultado = 1/resultado;
+        }
+        else{
+
+            resultado = 1;
         }
 
-        Console.WriteLine("Resultado = " + resultado);
+        Console.WriteLine(resultado);
     }
 }
