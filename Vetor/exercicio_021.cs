46afd7b67ee3bc4285cd25b900524071e7f8e943
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-02-21 17:04:25 -0300

    Update exercicio_021.cs

--- Vetor/exercicio_021.cs
+++ Vetor/exercicio_021.cs
@@ -3,11 +3,11 @@
 Relatório de Mouses
  
     Situação	                          Quantidade	Percentual
-    in
+    
 1- Necessita da esfera	                    40	        40%
 2- Necessita de limpeza	                    30	        30%
-3- Necessita troca do cabo ou conector	     15	        15%
-4- Quebrado ou inutilizado	               15	        15%
+3- Necessita troca do cabo ou conector	    15	        15%
+4- Quebrado ou inutilizado	                15	        15%
 */
 
 using System;
@@ -17,17 +17,48 @@ class exercicio_021{
     static void Main(){
 
         int[] contador = new int[4];
-        int percentual = 0;
+        int[] percentual = new int[4];
         int total = 0;
         int op = 0;
 
         for (int i = 0; i < 200; i++){
             
-            Console.WriteLine("Informe a situacao do mouse " + (i + 1));
-            Console.WriteLine("1- Necessita da esfera\n2- Necessita de limpeza\n3- Necessita troca do cabo ou conecto\n4- Quebrado ou inutilizado");
-            op = int.Parse(Console.ReadLine());
-            contador[op]++;
+            do{
+                
+                Console.WriteLine("Informe a situacao do mouse " + (i + 1));
+                Console.WriteLine("1- Necessita da esfera\n2- Necessita de limpeza\n3- Necessita troca do cabo ou conecto\n4- Quebrado ou inutilizado");
+                op = int.Parse(Console.ReadLine());
+
+                if (op > 4 || op < 0){
+                    
+                    Console.WriteLine("ERRO: valor invalido\n");
+                }
+
+            } while (op > 4 || op < 0);
+            
+            if (op == 0){
+                break;
+            }
+
+            contador[op - 1]++;
         }
+
+        for (int i = 0; i < 4; i++){
+            
+            total += contador[i];
+        }
+
+        for (int i = 0; i < 4; i++){
+        
+            percentual[i] = (contador[i] * 100) / total;
+        }
+
+        Console.WriteLine("Relatorio de mouses\n");
+        Console.WriteLine("    Situacao	                          Quantidade	Percentual\n");
+        Console.WriteLine("1- Necessita da esfera	                 " + contador[0] +	"	             " + percentual[0]  + "%");
+        Console.WriteLine("2- Necessita de limpeza	                 " + contador[1] +	"	             " + percentual[1]  + "%");
+        Console.WriteLine("3- Necessita troca do cabo ou conector    " + contador[2] +	"	             " + percentual[2]  + "%");
+        Console.WriteLine("4- Quebrado ou inutilizado	             " + contador[3] +	"	             " + percentual[3]  + "%");
 
     }
 }
