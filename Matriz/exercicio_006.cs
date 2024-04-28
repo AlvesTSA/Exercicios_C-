8d07b267d079561fdcd33d0bd10ce2d2cbd9a75a
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-04-28 17:50:39 -0300

    Create exercicio_006.cs

---
+++ Matriz/exercicio_006.cs
@@ -0,0 +1,40 @@
+/*6. Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais elementos. Escreva ao final a matriz obtida.*/
+
+using System;
+
+public class exercicio_006{
+
+    static void Main(){
+
+        const int size = 5;
+        int[,] matriz = new int[size,size];
+
+        for (int i = 0; i < size; i++){
+        
+            for (int y = 0; y < size; y++){
+                
+                if(i == y){
+
+                    matriz[i,y] = 1;
+                }
+                else{
+
+                    matriz[i,y] = 0;
+                }
+            }
+        }
+
+        Console.WriteLine("Matriz resultante:");
+        
+        for (int i = 0; i < size; i++){
+            
+            for (int y = 0; y < size; y++){
+                
+                Console.Write(matriz[i,y] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
