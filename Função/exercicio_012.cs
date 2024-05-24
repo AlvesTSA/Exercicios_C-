ac75ceba22badbe522cfb403fec62c7e2d24651d
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-05-24 10:52:00 -0300

    Create exercicio_012.cs

---
+++ Função/exercicio_012.cs
@@ -0,0 +1,39 @@
+﻿/*12. Faça uma função que leia um número não determinado de valores positivos e retorna a média aritmética dos mesmos.*/
+
+using System;
+
+class exercicio_012
+{
+    static void Main()
+    {
+        float media = MediaAritmetica();
+        string media_f = media.ToString("F2");
+        Console.WriteLine("Media aritmetica: {0}",media_f);
+    }
+
+    static float MediaAritmetica()
+    {
+        float media = 0.0F;
+        float numero = 0.0F;
+        int count = 0;
+
+        Console.WriteLine("Informe numeros inteiros ou informe -1 para sair:");
+
+        do
+        {
+            numero = float.Parse(Console.ReadLine());
+
+            if (numero == -1)
+            {
+                break;
+            }
+            
+            media += numero;
+            count++;
+            
+        } while (numero != -1);
+
+        return media / count;
+    }
+
+}
