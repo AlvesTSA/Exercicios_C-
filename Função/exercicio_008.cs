59b13a8f28cc5d098abaa23ed6a2a9e3eed0f495
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-05-24 10:51:48 -0300

    Create exercicio_008.cs

---
+++ Função/exercicio_008.cs
@@ -0,0 +1,25 @@
+﻿/*8. Faça uma função que recebe um valor inteiro e verifica se o valor é par ou ímpar. A função deve retornar um valor booleano*/
+using System;
+class exercicio_008
+{
+    static void Main()
+    {
+        int num = 0;
+
+        Console.Write("Informe um numero inteiro: ");
+        num = int.Parse(Console.ReadLine());
+
+        if (Impar_par(num))
+        {
+            Console.WriteLine("O numero informado é PAR");
+        }
+        else
+        {
+            Console.WriteLine("O numero informado é IMPAR");
+        }
+    }
+    static bool Impar_par(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
