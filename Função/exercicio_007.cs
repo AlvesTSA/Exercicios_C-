028f4dc5e8b54e737e0844533725f08a728cf539
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-05-24 10:51:39 -0300

    Create exercicio_007.cs

---
+++ Função/exercicio_007.cs
@@ -0,0 +1,27 @@
+﻿/*7. Faça uma função que recebe um valor inteiro e verifica se o valor é positivo ou negativo. A função deve retornar um valor booleano.*/
+
+using System;
+class exercicio_007
+{
+    static void Main()
+    {
+        int num = 0;
+    
+        Console.Write("Informe um valor inteiro: ");
+        num = int.Parse(Console.ReadLine());
+
+        if (Positivo_negativo(num))
+        {
+            Console.WriteLine("O valor informado é positivo.");
+        }
+        else
+        {
+           Console.WriteLine("O valor informado é negativo.");
+        }
+    }
+
+    static bool Positivo_negativo(int numero)
+    {
+        return numero > 0;
+    }
+}
