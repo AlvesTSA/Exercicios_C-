e6200331040a0f0f2996c8ca67dd574e24422cab
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-05-25 10:31:52 -0300

    Create exercicio_019.cs

---
+++ Função/exercicio_019.cs
@@ -0,0 +1,43 @@
+﻿/*19. Faça um programa para imprimir:
+
+1
+1   2
+1   2   3
+.....
+1   2   3   ...  n
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro imprima até a n-ésima linha.*/
+
+using System;
+
+class exercicio_019
+{
+    static void Main()
+    {
+        int n = 0;
+
+        do
+        {
+            Console.Write("Informe um valor inteiro: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n <= 0) 
+            {
+                Console.WriteLine("Erro: o número deve ser inteiro e positivo.");
+            }
+
+        } while (n <= 0);
+
+        Imprimir(n);
+    }
+    static void Imprimir(int n)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("{0} ",j);
+            }
+            Console.WriteLine();
+        }      
+    }
+}
